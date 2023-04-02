747650cc7f6674c6206908a253c5fdd954320ab4
Filip Farkas <ffarkas@c2r1s5.42prague.com>
2023-04-02 11:53:02 +0200

    Pokemons modified

--- ex11/find.py
+++ ex11/find.py
@@ -1,25 +1,25 @@
 import requests
 POKEAPI_WEB = "https://pokeapi.co/api/v2"
 
-def import_abilities(poke_name):
+def import_poke_abilities(poke_name):
     poke_web = POKEAPI_WEB + "pokemon/" + poke_name.lower()
-    response = requests.get(poke_web)
+    download = requests.get(poke_web)
 
-    if response.status_code == 200:
-        poke_data = response.json()
-        abilities = []
+    if download.status_code == 200:
+        poke_data = download.json()
+        poke_abilities = []
         for ability in poke_data["abilities"]:
-            abilities.append(ability["ability"]["name"])
-        return (abilities)
+            poke_abilities.append(ability["ability"]["name"])
+        return (poke_abilities)
     else:
         return None
 
 poke_name = input("Enter the name of a Pokemon: ")
-abilities = import_abilities(poke_name)
-if abilities:
+poke_abilities = import_poke_abilities(poke_name)
+if poke_abilities:
     print(f"Name: {poke_name.title()}")
     print(f"Abilities:")
-    for ability in abilities:
+    for ability in poke_abilities:
         print("- " + ability.title())
 else:
-    print(f"Could not retrieve abilities of {poke_name.title()}.")
+    print(f"Could not retrieve abilities of {poke_name.title()}. Check the name and try again.")
